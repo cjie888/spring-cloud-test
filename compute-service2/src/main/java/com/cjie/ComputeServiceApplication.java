aff8913260297d412758e8f460041ee77164e8d7
hucj <chengjiehu@creditease.cn>
2017-09-18 16:42:55 +0800

    add compute service

---
+++ compute-service2/src/main/java/com/cjie/ComputeServiceApplication.java
@@ -0,0 +1,17 @@
+package com.cjie;
+/**
+ * Created by hucj on 2017/9/18.
+ */
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+    }
+
+}
