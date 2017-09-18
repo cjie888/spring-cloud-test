48edf26432a9b74a6738a91d5edef9b618c05a41
hucj <chengjiehu@creditease.cn>
2017-09-18 16:10:32 +0800

    eureka server

---
+++ eruka-server/src/main/java/com/cjie/Application.java
@@ -0,0 +1,19 @@
+package com.cjie;
+/**
+ * Created by hucj on 2017/9/18.
+ */
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
