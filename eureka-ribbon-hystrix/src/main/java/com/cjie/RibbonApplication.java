f0b7190c75ac1e03f4650db91303221f030d92a7
hucj <chengjiehu@creditease.cn>
2017-09-22 09:34:16 +0800

    eureka ribbon hystrix

---
+++ eureka-ribbon-hystrix/src/main/java/com/cjie/RibbonApplication.java
@@ -0,0 +1,29 @@
+package com.cjie;
+
+/**
+ * Created by hucj on 2017/9/20.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class RibbonApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+}
