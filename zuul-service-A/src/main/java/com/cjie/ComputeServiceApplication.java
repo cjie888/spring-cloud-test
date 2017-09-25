489168edc823f1a7c8412caef2fccf22101b8015
cjie888 <cjie888@163.com>
2017-09-25 10:52:09 +0800

    zuul service

---
+++ zuul-service-A/src/main/java/com/cjie/ComputeServiceApplication.java
@@ -0,0 +1,18 @@
+package com.cjie;
+
+/**
+ * Created by hucj on 2017/9/25.
+ */
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+    }
+
+}
