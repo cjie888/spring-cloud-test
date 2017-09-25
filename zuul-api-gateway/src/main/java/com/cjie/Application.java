51dddd95c9d9eec8bbfc15512876689f9b6ef2d7
cjie888 <cjie888@163.com>
2017-09-25 19:26:33 +0800

    zuul api gate way

---
+++ zuul-api-gateway/src/main/java/com/cjie/Application.java
@@ -0,0 +1,25 @@
+package com.cjie;
+
+/**
+ * Created by hucj on 2017/9/25.
+ */
+import com.cjie.filter.AccessFilter;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
+
+}
